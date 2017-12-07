39e2428ea19e8b2279b8c500ea6168060bb66e98
vipragupta <guptavipra@gmail.com>
2017-12-07 04:46:14 -0700

    ip to host cache working, but slowing down

--- webproxy.c
+++ webproxy.c
@@ -8,13 +8,53 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <errno.h>
+#include <fcntl.h>
+
+#define ipHostCacheFile "htimhtim"
    
 void error(char* msg)
 {
   perror(msg);
   exit(0);
 }
+
+int getHostIP(char* hostMap[], char* host, int maxIndex) {
+  printf("\n");
+  printf("host: %s\n", host);
   
+  for (int i = 0; i < maxIndex; i++) {
+    if (strcmp(hostMap[i], host) == 0) {
+      printf("Returning: %d\n\n", i);
+      return i;
+    }
+  }
+  printf("Returning: -1\n\n");
+  return -1;
+}
+  
+int writeFile(char *fileName, char *data, int size, int append) {
+  FILE *file;
+  char fileNameW[50];
+  bzero(fileNameW, sizeof(fileNameW));
+
+  strcpy(fileNameW, fileName);
+
+  if (append == 0) {
+    file = fopen(fileNameW,"wb");
+  } else {
+    file = fopen(fileNameW,"ab");
+  }
+
+  int fileSize = fwrite(data , sizeof(unsigned char), size, file);
+
+  if(fileSize < 0)
+    {
+      printf("Error writting file\n");
+        exit(1);
+    }
+    fclose(file);
+}
+
 int main(int argc, char* argv[])
 {
   pid_t pid;
@@ -68,6 +108,35 @@ int main(int argc, char* argv[])
     {
       printf("******************************************************************************************\n");
 
+      FILE *file;
+      char data[1024*1024];
+      char* hostMap[50];
+      char* ipMap[50];
+  
+      for (int i=0; i < 50; i++) {
+        hostMap[i] = calloc(50, sizeof(char));
+        ipMap[i] = calloc(50, sizeof(char));
+      }   
+
+      file = fopen(ipHostCacheFile, "r");
+      int index = 0;
+
+      if (file) {
+        while (fgets(data, sizeof(data), file)) {
+          char *tok = strtok(data, "\n");
+          char *tokkk = strtok(tok, " ");
+
+          if (tokkk != NULL) {
+            strcpy(hostMap[index],tokkk);
+            char *temp = strtok(NULL, " ");
+            strcpy(ipMap[index],temp);
+            
+            index++;
+          }
+        }
+      }
+      fclose(file);
+
       struct sockaddr_in server_addr;
       int flag = 0, recvsocketId, n,port = 0, i,socketId;
       char buffer[510], httpMethod[300], hostStr[300], httpVersion[10];
@@ -108,8 +177,38 @@ int main(int argc, char* argv[])
         }
            
         sprintf(hostStr, "%s", temp);
-        //printf("host: %s\n", hostStr);
-        host = gethostbyname(hostStr);
+        printf("host: %s\n", hostStr);
+
+        int pass = getHostIP(hostMap, hostStr, index);
+
+        for (int i = 0; i < index; i++) {
+          printf("i:%d     host:%s     ip:%s\n", i, hostMap[i], ipMap[i]);
+        }
+
+        if (pass == -1) {
+
+          host = gethostbyname(hostStr);
+          
+          strcpy(hostMap[index], hostStr);
+          bcopy( (char*)host->h_addr, (char*)ipMap[index], host->h_length);
+          
+          
+          pass = index;
+          index++;
+
+          char ipHostCacheData[200];
+          bzero(ipHostCacheData, sizeof(ipHostCacheData));
+          strcpy(ipHostCacheData, hostMap[index-1]);
+          strcat(ipHostCacheData, " ");
+          strcat(ipHostCacheData, ipMap[index-1]);
+          strcat(ipHostCacheData, "\n");
+          ipHostCacheData[strlen(ipHostCacheData)] = '\0';
+          printf("str: %s   host: %s     ip: %s    index: %d\n", hostStr,  hostMap[pass], ipMap[pass], pass);
+          writeFile(ipHostCacheFile, ipHostCacheData, strlen(ipHostCacheData), 1);
+        } else {
+          printf("FOUNDDDDDDDDDD\n");
+          printf("str: %s   host: %s     ip: %s    index: %d\n", hostStr,  hostMap[pass], ipMap[pass], pass);
+        }
            
         if(flag == 1)
         {
@@ -124,14 +223,16 @@ int main(int argc, char* argv[])
         if(temp != NULL) {
           temp = strtok(NULL, "^]");
         }
-        //printf("path: %s\n",  temp);
-        //printf("port: %d\n",  port);
+        printf("path: %s\n",  temp);
+        printf("port: %d\n",  port);
            
            
         bzero((char*)&server_addr, sizeof(server_addr));
         server_addr.sin_port=htons(port);
         server_addr.sin_family=AF_INET;
-        bcopy((char*)host->h_addr, (char*)&server_addr.sin_addr.s_addr, host->h_length);
+        strcpy((char*)&server_addr.sin_addr.s_addr, (char*)ipMap[pass]);
+
+        //bcopy((char*)host->h_addr, (char*)&server_addr.sin_addr.s_addr, host->h_length);
            
         socketId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         recvsocketId = connect(socketId, (struct sockaddr*)&server_addr, sizeof(struct sockaddr));
@@ -151,7 +252,8 @@ int main(int argc, char* argv[])
         else {
           sprintf(buffer, "GET http://%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n", hostStr, httpVersion, hostStr);
         }
-         
+        
+        printf("\n\n%s\n", buffer);
          
         n = send(socketId, buffer, strlen(buffer), 0);
         printf("\n%s\n", buffer);
